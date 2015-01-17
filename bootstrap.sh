e5516a961d31ca32b1eaa917dff7e31141a02bb4
Tim Dennis <dennis.tim@gmail.com>
2015-01-17 14:17:24 -0800

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo "Provisioning virtual maching for MySQL workshop"
+
+apt-get update && apt-get dist-upgrade
+
+#rstudio
+echo "Installing R & RStudio"
+
+apt-get install -y gdebi-core
+apt-get install -y r-base-core
+apt-get install -y libapparmor1
+apt-get install -y openssl
+wget -q http://download1.rstudio.org/rstudio-0.98.1091-amd64.deb
+gdebi --non-interactive rstudio-server-0.98.1091-amd64.deb
+
+#mysql
+echo "Preparing MySQL"
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y mysql-server
+
+cat << EOF | sudo tee -a /etc/mysql/conf.d/default_engine.cnf
+[mysqld]
+default-storage-engine = MyISAM
+EOF
+
+echo "Installing MySQL"
+
+sudo service mysql restart
+
+echo "You've been provisioned"
